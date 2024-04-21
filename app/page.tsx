838eb60b4ce29c3ec8e4c0069b18ee0bab4a9649
Robert Engel <robert.engel@apollidon.com>
2024-04-21 18:29:31 -0400

    Adding tailwind and setting up basic grid of boxes.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,42 @@
+'use client';
 import * as React from 'react';
+import { useState } from 'react';
+
+const lifeSimulation = [
+  [1, 1, 0, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 0, 0, 0, 1, 0, 1, 1],
+  [0, 1, 0, 0, 0, 0, 1, 0, 1, 0],
+  [0, 0, 0, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 0, 0, 0, 1, 0, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+];
 
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  const data = getData();
+  const [population, setPopulation] = useState(data);
+
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p>{population.length}</p>
+      <div className={'flex flex-col'}>
+        {population.map((row, rowIdx) => (
+          <div key={rowIdx} className={'flex-row'}>
+            {row.map((node, nodeIdx) => (
+              <input
+                key={nodeIdx}
+                id={`${rowIdx}-${nodeIdx}`}
+                type={'checkbox'}
+                checked={node === 1}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function getData() {
+  return lifeSimulation;
 }
--- index.ts
+++ index.ts
@@ -4,18 +4,26 @@ import { getNodeOutput } from '@/src/getNodeConsoleOutput';
 import { getColsArg, getGenerationsArg, getRowsArg } from '@/src/getArgs';
 import { getGrid } from '@/src/getGrid';
 
-const ROWS = getRowsArg(process.argv);
-const COLS = getColsArg(process.argv);
+export function gameOfLife(rows?: number, cols?: number, gens?: number) {
+  const ROWS = rows ? rows : getRowsArg(process.argv);
+  const COLS = cols ? cols : getColsArg(process.argv);
+  const GENS = gens ? gens : getGenerationsArg(process.argv, 3);
 
-// Initializing the grid.
-let grid = getGrid(ROWS, COLS);
+  // Initializing the grid.
+  const grids = [];
+  let grid = getGrid(ROWS, COLS);
 
-process.stdout.write('Hello, RAE Dev Game of Life! \n' + os.EOL);
-/// Run the simulation for the given number of generations
-for (let i = 0; i < getGenerationsArg(process.argv, 3); i++) {
-  let output = '';
-  process.stdout.write('Generation ' + (i + 1) + os.EOL);
-  grid = nextGeneration(grid, ROWS, COLS);
-  output = getNodeOutput(grid, ROWS, COLS);
-  process.stdout.write(output + os.EOL);
+  process.stdout.write('Hello, RAE Dev Game of Life! \n' + os.EOL);
+  /// Run the simulation for the given number of generations
+  for (let i = 0; i < GENS; i++) {
+    let output = '';
+    process.stdout.write('Generation ' + (i + 1) + os.EOL);
+    grid = nextGeneration(grid, ROWS, COLS);
+    grids.push(grid);
+    output = getNodeOutput(grid, ROWS, COLS);
+    process.stdout.write(output + os.EOL);
+  }
+
+  return grids;
 }
+gameOfLife();
